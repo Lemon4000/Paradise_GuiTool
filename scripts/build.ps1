c19bdb3dcefbd739fa1dbf031e612c5573006582
Lemon <1692184234@qq.com>
2025-12-25 10:52:54 +0800

    更新构建

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -8,12 +8,12 @@ if ($OneFile) { $mode = "-F" }
 
 if (Test-Path "$Name.spec") { Remove-Item "$Name.spec" -Force -ErrorAction SilentlyContinue }
 
-python -m PyInstaller $mode --noconsole --icon "ICON.png" -n $Name gui/main.py --add-data "config;config" --clean
+python -m PyInstaller $mode --noconsole --icon "ICON.png" -n $Name gui/main.py --add-data "config;config" --hidden-import serial.tools.list_ports_windows --hidden-import serial.tools.list_ports --clean
 
 if ($OneFile) {
   $distDir = "dist"
   # Rebuild with local runtime tmp dir to avoid embedded interpreter startup issues
-  python -m PyInstaller -F --noconsole --icon "ICON.png" -n $Name gui/main.py --add-data "config;config" --runtime-tmpdir . --clean
+  python -m PyInstaller -F --noconsole --icon "ICON.png" -n $Name gui/main.py --add-data "config;config" --hidden-import serial.tools.list_ports_windows --hidden-import serial.tools.list_ports --runtime-tmpdir . --clean
   if (Test-Path "config") { Copy-Item -Recurse -Force "config" (Join-Path $distDir "config") }
 } else {
   $distDir = Join-Path "dist" $Name
